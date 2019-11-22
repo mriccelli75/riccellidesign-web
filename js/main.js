5835a4151f27c9b54bc7a8f540f16c6f5ade84a5
Mike Riccelli <mriccelli@ons.org>
2019-11-22 11:45:43 -0500

    design work for the homepage

---
+++ js/main.js
@@ -0,0 +1,36 @@
+ /* =========================================
+     *  animations
+     *  =======================================*/
+
+    delayTime = 0;
+
+    $('[data-animate]').waypoint(function (direction) {
+        delayTime += 250;
+
+        var element = $(this.element);
+
+        $(this.element).delay(delayTime).queue(function (next) {
+            element.toggleClass('animated');
+            element.toggleClass(element.data('animate'));
+            delayTime = 0;
+            next();
+        });
+
+        this.destroy();
+
+    }, {
+        offset: '90%'
+    });
+    
+    $('[data-animate-hover]').hover(function () {
+        $(this).css({
+            opacity: 1
+        });
+        $(this).addClass('animated');
+        $(this).removeClass($(this).data('animate'));
+        $(this).addClass($(this).data('animate-hover'));
+    }, function () {
+        $(this).removeClass('animated');
+        $(this).removeClass($(this).data('animate-hover'));
+    });
+    
