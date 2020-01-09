a22ccfddce5b0c5f71a3cc6a338822e2a9b0ace1
Mike Riccelli <mriccelli@ons.org>
2020-01-09 10:56:25 -0500

    new pages and css added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+  
  /* =========================================
      *  animations
      *  =======================================*/
@@ -29,9 +30,18 @@
       delay: 1000,
       origin:'bottom'
     });
+    sr.reveal('#about-me .intro-content', {
+      duration: 3000,
+      origin:'left',
+      distance:'300px'
+    });
+   
+    
     // Headline still
     ScrollReveal().reveal('#capabilities .title')
     ScrollReveal().reveal('#capabilities .capabilities-web', {duration: 1000, delay: 500 })
     ScrollReveal().reveal('#capabilities .capabilities-ecom', {duration: 2000, delay: 600 })
     ScrollReveal().reveal('#capabilities .capabilities-ux', {duration: 3000, delay: 700 })
+    ScrollReveal().reveal('#call-to-action .title.lined', {duration: 3000, delay: 700 })
+    ScrollReveal().reveal('#call-to-action .btn.btn-primary', {duration: 3000, delay: 800 })
 
